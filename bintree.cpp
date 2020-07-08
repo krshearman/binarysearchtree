476184eba64f586eb04f7f08cd0c42aa0df21a39
kendall <kendall.shearman@g.austincc.edu>
2020-07-08 09:43:06 -0500

    //Added comment headers. Stubbed out public and private methods.

--- bintree.cpp
+++ bintree.cpp
@@ -1,6 +1,108 @@
-/*
- * Your comment header here
- */
+/*********************
+Name: Kendall Shearman
+Coding 06
+Purpose: Binary Tree
+**********************/
 
 #include "bintree.h"
 
+//public methods
+BinTree::BinTree(){
+    count = 0;
+    DataNode * root = nullptr;
+}
+
+BinTree::~BinTree(){
+
+}
+
+bool BinTree::isEmpty(){
+    return false;
+}
+
+int BinTree::getCount(){
+    return 0;
+}
+
+bool BinTree::getRootData(Data * data){
+    return false;
+}
+
+void BinTree::displayTree(){
+
+}
+
+void BinTree::clear(){
+
+}
+
+bool BinTree::addNode(int id, string data){
+    return false;
+}
+
+bool BinTree::removeNode(int id){
+    return false;
+}
+
+bool BinTree::getNode(Data * data, int id){
+    return false;
+}
+
+bool BinTree::contains(int id){
+    return false;
+}
+
+int BinTree::getHeight(){
+    return 0;
+}
+
+void BinTree::displayPreOrder(){
+
+}
+
+void BinTree::displayPostOrder(){
+
+}
+
+void BinTree::displayInOrder(){
+
+}
+
+//private methods
+
+void BinTree::clear(DataNode*){
+
+}
+
+bool BinTree::addNode(DataNode * data, DataNode ** dataptr){
+    return false;
+}
+
+DataNode* BinTree::removeNode(int id, DataNode * data){
+    DataNode * ptr;
+    return ptr;
+}
+
+bool BinTree::getNode(Data * data, int id, DataNode * ptr){
+    return false;
+}
+
+bool BinTree::contains(int id, DataNode * data){
+    return false;
+}
+
+int BinTree::getHeight(DataNode * data){
+    return 0;
+}
+
+void BinTree::displayPreOrder(DataNode * data){
+
+}
+
+void BinTree::displayPostOrder(DataNode * data){
+
+}
+
+void BinTree::displayInOrder(DataNode * data){
+
+}
--- bintree.h
+++ bintree.h
@@ -1,9 +1,46 @@
-/*
- * Your comment header here
- */
+/*********************
+Name: Kendall Shearman
+Coding 06
+Purpose: Binary Tree
+**********************/
 
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
+#include "data.h"
+
+class BinTree {
+
+public:
+    BinTree();
+    ~BinTree();
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+    void clear();
+    bool addNode(int, string);
+    bool removeNode(int);
+    bool getNode(Data*, int);
+    bool contains(int);
+    int getHeight();
+    void displayPreOrder();
+    void displayPostOrder();
+    void displayInOrder();
+
+private:
+    void clear(DataNode*);
+    bool addNode(DataNode*, DataNode**);
+    DataNode* removeNode(int, DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+    void displayPreOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    void displayInOrder(DataNode*);
+
+    DataNode *root;
+    int count;
+};
 
 
 
